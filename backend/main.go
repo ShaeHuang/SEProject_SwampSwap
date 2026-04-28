1c2fb450b47d8b3d714e19112a0853848d1e9621
Kabeer <kabeerlatane@ufl.edu>
2026-04-28 08:37:56 -0400

    feat: register messaging routes and migrate Message model

--- backend/main.go
+++ backend/main.go
@@ -61,6 +61,9 @@ func main() {
 		protected.PUT("/user", UpdateUser)
 		protected.PUT("/listings/:id/buy", BuyListing)
 		protected.POST("/avatar", uploadAvatar)
+		protected.GET("/messages", GetConversations)
+		protected.GET("/messages/:userId", GetMessagesWithUser)
+		protected.POST("/messages", SendMessage)
 	}
 
 	err := r.Run(":8080")
@@ -78,5 +81,5 @@ func ConnectDatabase() {
 	}
 
 	DB = database
-	DB.AutoMigrate(&User{}, &Listing{})
+	DB.AutoMigrate(&User{}, &Listing{}, &Message{})
 }
